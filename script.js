0a4cb477ca11bf8b9266d180cac3c63a974eb9be
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-06-18 08:35:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+
+document.getElementById('change_button').addEventListener('click', () => {
+  const blockId = document.getElementById('block_id').value;
+  const color = document.getElementById('colour_id').value;
+
+
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById(i).style.backgroundColor = 'transparent';
+  }
+
+
+  const selectedBlock = document.getElementById(blockId);
+  if (selectedBlock) {
+    selectedBlock.style.backgroundColor = color;
+  }
+});
+
+document.getElementById('Reset').addEventListener('click', () => {
+  for (let i = 1; i <= 9; i++) {
+    document.getElementById(i).style.backgroundColor = 'transparent';
+  }
+});
